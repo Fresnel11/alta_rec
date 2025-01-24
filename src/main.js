809b637f1ec35a0dabc1bd5b4dc73a043e36f5f3
fresnel jeanclaude <fresneljeanclaudecossou64@gmail.com>
2025-01-24 11:48:58 +0100

    fresnel

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import PrimeVue from 'primevue/config';
 import './style.css'
 import App from './App.vue'
 import router from './router'
@@ -8,5 +7,4 @@ import pinia from './store'
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
-app.use(PrimeVue);
 app.mount('#app')
